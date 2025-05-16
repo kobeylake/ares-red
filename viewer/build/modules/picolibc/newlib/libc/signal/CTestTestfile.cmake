36a0053ef7fc1761c748b5c2abddcd6bfb55b1b0
james lowe <s4744257@student.uq.edu.au>
2025-05-16 15:14:35 +1000

    git ignore uploaded

--- viewer/build/modules/picolibc/newlib/libc/signal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/csse4011/repo/modules/lib/picolibc/newlib/libc/signal
-# Build directory: /home/james/csse4011/repo/mycode/apps/prac3/viewer_node/build/modules/picolibc/newlib/libc/signal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
